8282cc360e70b21141c17f3580906f45dc2991cc
Justin Kramer <jpk91@pitt.edu>
2019-03-02 14:50:17 -0500

    Testing text file

--- check.php
+++ check.php
@@ -1,5 +1,1 @@
- <?php
-$myfile = fopen("https://docs.google.com/spreadsheets/d/e/2PACX-1vRrYePMLyI58kvYWZ2icXi3Fe8LZxUNS29clJfwoz3Jjh5mMwMoJemOzMz86fkJoBYtkRcK8fs4JUms/pub?output=tsv", "r") or die("Unable to open file!");
-echo "Hey";
-fclose($myfile);
-?> 
+<?php echo '<p>Hello World</p>'; ?>
