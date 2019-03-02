23f8bb0f1f23ce53d50dd130ed2f4f0aa0a4bde5
Sam Rosenberg <serosenberg99@gmail.com>
2019-03-02 13:18:03 -0500

    Updated algorithm and csv's

--- draftgenie.py
+++ draftgenie.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
@@ -15,8 +16,9 @@ def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
     mae = mean_absolute_error(val_y, preds_val)
     return(mae)
 
+null_columns = ["MPG","PPG","RPG","APG","WS/48","BPM","VORP"]
+
 def get_rid_of_nulls(train_data):
-	null_columns = ["MPG","PPG","RPG","APG","WS","BPM","VORP"]
 	#print(train_data[null_columns])
 	for pick in range(1,61):
 		check_for_empty = train_data.loc[train_data["Pk"]==pick]
@@ -37,31 +39,57 @@ def get_rid_of_nulls(train_data):
 train_data = pd.read_csv("no_empties.csv")
 train_data = train_data.dropna(axis=0)
 
-features = ["MPG","PPG","RPG","APG","WS","BPM","VORP"]
+#print(train_data[null_columns].describe())
+
+features = ["MPG","PPG","RPG","APG","WS/48","BPM","VORP"]
 X = train_data[features]
 y = train_data.Pk
 
 train_X,val_X,train_y,val_y = train_test_split(X,y,random_state=0)
 
 ###Determine best number of nodes
-#candidate_max_leaf_nodes =[5,10,15,25,50,100,250,500]
-#scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
-#best_tree_size = min(scores, key=scores.get) #50
+candidate_max_leaf_nodes =[6,7,8,9,10,11,12,13,14,15]
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+best_tree_size = min(scores, key=scores.get)
 #print(best_tree_size)
 
-train_model = DecisionTreeRegressor(max_leaf_nodes=5, random_state=1)
-train_model.fit(train_X,train_y)
-train_pred = train_model.predict(val_X)
+train_model = DecisionTreeRegressor(max_leaf_nodes=60,random_state=1)
+train_model.fit(X,y)
+train_pred = train_model.predict(X)
+#print(train_pred)
 
 #train_model = RandomForestRegressor(random_state=1)
 #train_model.fit(train_X,train_y)
 #train_pred = train_model.predict(val_X)
 #print(train_pred)
 
-#train_mae = mean_absolute_error(train_pred,val_y)
+#train_mae = mean_absolute_error(train_pred,y)
 #print(train_mae)
 
+mpg= random.randint(2,41)
+if mpg==None:
+	mpg = train_data["MPG"].mean()
+ppg= random.randint(0,27)
+if ppg==None:
+	ppg = train_data["PPG"].mean()
+rpg= random.randint(0,13)
+if rpg==None:
+	rpg = train_data["RPG"].mean()
+apg= random.randint(0,9)
+if apg==None:
+	apg = train_data["APG"].mean()
+ws48= random.uniform(-.597,1.367)
+if ws48==None:
+	ws48 = train_data["WS/48"].mean()
+bpm=  random.randint(-23,19)
+if bpm==None:
+	bpm = train_data["BPM"].mean()
+vorp= random.randint(-8,89)
+if vorp==None:
+	vorp = train_data["VORP"].mean()
 
-test_X = [30,20,5,3,1,.5,0]
+
+print("MPG:{} PPG:{} RPG:{} APG:{} WS:{:.2f} BPM:{} VORP:{} ".format(mpg,ppg,rpg,apg,ws48,bpm,vorp))
+test_X = ([[mpg,ppg,rpg,apg,ws48,bpm,vorp]])
 test_pred = train_model.predict(test_X)
 print(int(test_pred))
