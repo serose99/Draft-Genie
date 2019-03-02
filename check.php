cb44331a0a36a922dd4643b43d8dbeb554fc0696
Justin Kramer <jpk91@pitt.edu>
2019-03-02 14:49:09 -0500

    Testing text file

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
  <?php
 $myfile = fopen("https://docs.google.com/spreadsheets/d/e/2PACX-1vRrYePMLyI58kvYWZ2icXi3Fe8LZxUNS29clJfwoz3Jjh5mMwMoJemOzMz86fkJoBYtkRcK8fs4JUms/pub?output=tsv", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("https://docs.google.com/spreadsheets/d/e/2PACX-1vRrYePMLyI58kvYWZ2icXi3Fe8LZxUNS29clJfwoz3Jjh5mMwMoJemOzMz86fkJoBYtkRcK8fs4JUms/pub?output=tsv"));
+echo "Hey";
 fclose($myfile);
 ?> 
